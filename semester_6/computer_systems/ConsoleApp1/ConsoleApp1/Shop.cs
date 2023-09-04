8354e2a5d9ba431073c39064e8f183ec36b50195
ShpriZZ <Dzerenus@ya.ru>
2023-09-04 22:16:29 +0900

    Добавлены экспертные системы

---
+++ semester_6/computer_systems/ConsoleApp1/ConsoleApp1/Shop.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp1;
+
+public class Shop
+{
+    public int Id { get; }
+
+    public Shop(int id)
+    {
+        Id = id;
+    }
+
+    public string GetJson()
+    {
+        var jsonObject = new ShopJson(this);
+        return JsonSerializer.Serialize(jsonObject);
+    }
+
+    public static Shop FromJson(string json)
+    {
+        var jsonObject = JsonSerializer.Deserialize<ShopJson>(json);
+
+        if (jsonObject == null)
+            throw new NullReferenceException();
+
+        return jsonObject.GetModel();
+    }
+}
+
+
+public class ShopJson
+{
+    public int KakoeToEbanoeName { get; set; }
+
+    public ShopJson(Shop shop)
+    {
+        KakoeToEbanoeName = shop.Id;
+    }
+
+    public Shop GetModel()
+    {
+        return new Shop(KakoeToEbanoeName);
+    }
+}
